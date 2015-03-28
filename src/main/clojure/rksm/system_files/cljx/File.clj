96013f7b2cd3b393f1ba265d78b0169fe7021031
Robert Krahn <robert.krahn@gmail.com>
2015-03-28 04:58:48 -0700

    reading / writign cljx

--- src/main/clojure/rksm/system_files/cljx/File.clj
+++ src/main/clojure/rksm/system_files/cljx/File.clj
@@ -1,7 +1,7 @@
 (ns rksm.system-files.cljx.File
   (:require [clojure.java.io :as io]
             (cljx core rules))
-  (:import (java.io File FileInputStream))
+  (:import (java.io File FileInputStream FileOutputStream))
   (:gen-class :name rksm.system-files.cljx.File
               :extends java.io.File
               :main false
@@ -12,7 +12,9 @@
                              [java.io.File String] [java.io.File String]
                              [java.net.URI] [java.net.URI]}
               :methods [[changeMode [clojure.lang.Keyword] clojure.lang.Keyword]
-                        [getMode [] clojure.lang.Keyword]]))
+                        [getMode [] clojure.lang.Keyword]
+                        [getCljs [] String]
+                        [getClj [] String]]))
 
 ; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ; cljx file related
@@ -32,6 +34,28 @@
   [this]
   (some-> (.mode this) deref))
 
+(defn- input-stream-with-rule
+  [^rksm.system-files.cljx.File cljx-file cljx-rule & [opts]]
+  (let [rules (case cljx-rule
+                :cljx nil
+                :clj cljx.rules/clj-rules
+                :cljs cljx.rules/cljs-rules
+                cljx-rule)]
+    (if-not rules
+      (io/make-input-stream (FileInputStream. cljx-file) opts)
+      (-> (.getCanonicalPath cljx-file)
+        slurp (cljx.core/transform rules)
+        java.io.StringBufferInputStream.
+        io/input-stream))))
+
+(defn -getClj
+  [this]
+  (slurp (input-stream-with-rule this :clj)))
+
+(defn -getCljs
+  [this]
+  (slurp (input-stream-with-rule this :cljs)))
+
 (def output-modes [:cljx :cljs :clj])
 
 (def ^:dynamic *output-mode* nil)
@@ -45,18 +69,9 @@
   (assoc io/default-streams-impl
          :make-input-stream
          (fn [^rksm.system-files.cljx.File file opts]
-           (let [mode (output-mode file)
-                 rules (case mode
-                         :cljx nil
-                         :clj cljx.rules/clj-rules
-                         :cljs cljx.rules/cljs-rules
-                         mode)]
-             (if-not rules
-               (io/make-input-stream (FileInputStream. file) opts)
-               (-> (.getCanonicalPath file)
-                 slurp (cljx.core/transform rules)
-                 java.io.StringBufferInputStream.
-                 io/input-stream))))))
+           (input-stream-with-rule file (output-mode file) opts))
+         :make-output-stream (fn [^rksm.system-files.cljx.File x opts]
+                               (io/make-output-stream (io/file (.getPath x)) opts))))
 
 (defn with-mode
   [^String path ^clojure.lang.Keyword mode]
