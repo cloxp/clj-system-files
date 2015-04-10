f0c2fc0795fe2aaa88d2ef33e228e82ebc796dd5
Robert Krahn <robert.krahn@gmail.com>
2015-04-10 05:35:53 -0700

    require-ns supports cljs... for now

--- src/main/clojure/rksm/system_files/loading.clj
+++ src/main/clojure/rksm/system_files/loading.clj
@@ -2,8 +2,13 @@
   (:require [rksm.system-files.cljx :as cljx]))
 
 (defn require-ns
-  [ns-name & [file-name]]
-  (try 
-    (require ns-name :reload)
-    (catch java.io.FileNotFoundException e
-      (cljx/require-ns ns-name file-name))))
+  [ns-sym & [file-name]]
+  (if (and file-name (re-find #"\.cljs$" (str file-name)))
+    (do
+      ; FIXME... dependency to cloxp-cljs!!?
+      (require 'rksm.cloxp-cljs.ns.internals)
+      (rksm.cloxp-cljs.ns.internals/ensure-ns-analyzed! ns-sym file-name))
+    (try 
+      (require ns-sym :reload)
+      (catch java.io.FileNotFoundException e
+        (cljx/require-ns ns-sym file-name)))))
