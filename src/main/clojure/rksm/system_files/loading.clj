7984f8c6ed6a55fd6435246b4c1271e872cee9d0
Robert Krahn <robert.krahn@gmail.com>
2015-04-11 06:57:23 -0700

    remove cljs part from ns loading again

--- src/main/clojure/rksm/system_files/loading.clj
+++ src/main/clojure/rksm/system_files/loading.clj
@@ -3,12 +3,7 @@
 
 (defn require-ns
   [ns-sym & [file-name]]
-  (if (and file-name (re-find #"\.cljs$" (str file-name)))
-    (do
-      ; FIXME... dependency to cloxp-cljs!!?
-      (require 'rksm.cloxp-cljs.ns.internals)
-      (rksm.cloxp-cljs.ns.internals/ensure-ns-analyzed! ns-sym file-name))
-    (try 
-      (require ns-sym :reload)
-      (catch java.io.FileNotFoundException e
-        (cljx/require-ns ns-sym file-name)))))
+  (try 
+    (require ns-sym :reload)
+    (catch java.io.FileNotFoundException e
+      (cljx/require-ns ns-sym file-name))))
